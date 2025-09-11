28b3ca954da7cb86e0a39fe5c4e4f2cbb871f7af
Sunderam <kapoorsandy37@gmail.com>
2025-09-11 17:25:38 +0530

    added birthday message

--- src/App.js
+++ src/App.js
@@ -23,20 +23,13 @@ function App() {
     <div className="love-page">
       <Confetti width={windowSize.width} height={windowSize.height} numberOfPieces={25} />
       <div className="container">
-        <h2 className="fade-in float-in">Happy Girlfriend's Day, Rashi 💖</h2>
+        <h2 className="fade-in float-in">Happy Birthday Day, Rashi !!!</h2>
 
-        <p className="fade-in delay-1">From the moment you came into my life, everything changed for the better</p>
-        <p className="fade-in delay-2">Every day with you feels like a dream. You're the sunshine in my mornings, the peace in my evenings and joy in my heart.</p>
-        <p className="fade-in delay-3">I cannot wait for all memories we will make together in our future</p>
-        <p className="fade-in delay-4">You are my everything, today and always💫</p>
-        <p className="fade-in delay-4"><i><b>I LOVE YOU ❤️</b></i></p>
-        <p className="fade-in delay-4 signature">Forever yours,<br />Sunderam❤️</p>
-      </div>
-
-      <div className="floating-hearts">
-        {[...Array(15)].map((_, i) => (
-          <div key={i} className="heart" style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 10}s` }} />
-        ))}
+        <p className="fade-in delay-1">Wishing a very Happy Birthday to my favorite person !😇</p>
+        <p className="fade-in delay-2">Being your boyfriend is my best thing that has ever happened to me.😭</p>
+        <p className="fade-in delay-2">Thank you for being you - gorgeous, funny, and endlessly caring. I hope this year brings you as much happiness as you have brought into my life.💫</p>
+        <p className="fade-in delay-3">I hope you know, I will do everything I can to give you the ever lasting, true love you deserve 😘</p>
+        <h2 className="fade-in delay-4"><i><b>I LOVE YOU BABE 💖</b></i></h2>
       </div>
     </div>
   );
