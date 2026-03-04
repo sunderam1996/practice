188c19c158dfb9cab89bb581fc8eb86f5f4674be
sunderam1996 <sunderam96@gmail.com>
2026-03-04 19:21:13 +0530

    feat(word): updated App file

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <p className="fade-in delay-2">Thank you for every late-night call and every good morning text, every corset and towel pictures that almost give me a heart attack and every single time, you look at me like Iam your whole world, because you are mine. I love the next 6 months, the next 6 years, and all the months after that with you. </p>
         <p className="fade-in delay-3">Happy half-year of us, gorgeous. I love you more than yesterday, but nowhere near as much as tomorrow. </p>
         <p className="fade-in delay-3">I hope, you will try to forget your past and you will also love me completely 100% someday. </p>
-        <p className="fade-in delay-4">I wish, we go to Prague one day. I will be waiting for you with bag of Doritos and boarding pass at the airport, hoping that you would approach me, and we will fall in love once again.</p>
+        <p className="fade-in delay-4">I wish, we go to Prague one day. I will be waiting for you with bag of Doritos and boarding pass at the airport, hoping that you would approach me and we will fall in love once again.</p>
         <h2 className="fade-in delay-4"><i><b>Forever yours RASHI💖</b></i></h2>
         <h2 className="fade-in delay-4"><i><b>MENKA</b></i></h2>
       </div>
